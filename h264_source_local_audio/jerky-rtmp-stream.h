8ad5647388ebe7a1249af74e9907a2fc8db8407e
luzq <luzq@rgbvr.com>
2016-09-10 19:25:41 +0800

    add

--- h264_source_local_audio/jerky-rtmp-stream.h
+++ h264_source_local_audio/jerky-rtmp-stream.h
@@ -41,28 +41,30 @@ struct jerky_video_packet : jerky_av_packet
 };
 
 struct rtmp_stream {
-	struct jerky_circlebuf packets;
-	bool             sent_headers;
+	struct jerky_circlebuf	packets;
+	bool					sent_headers;
 
-	volatile bool    connecting;
+	volatile bool			connecting;
 
-	volatile bool    active;
-	volatile bool    disconnected;
-	pthread_t        send_thread;
+	volatile bool			active;
+	volatile bool			disconnected;
+	pthread_t				send_thread;
 
-	int              max_shutdown_time_sec;
+	int						max_shutdown_time_sec;
 
 
-	struct dstr      path, key;
-	struct dstr      username, password;
-	struct dstr      encoder_name;
-	struct dstr      bind_ip;
+	struct dstr				path, key;
+	struct dstr				username, password;
+	struct dstr				encoder_name;
+	struct dstr				bind_ip;
 
-	int64_t          last_dts_usec;
+	int64_t					last_dts_usec;
 
-	uint64_t         total_bytes_sent;
-	int              dropped_frames;
-	RTMP             rtmp;
+	uint64_t				total_bytes_sent;
+	int						dropped_frames;
+	jerky_av_packet			*videoSH;
+	jerky_av_packet			*audioSH;
+	RTMP					rtmp;
 };
 
 inline size_t num_buffered_packets(struct rtmp_stream *stream);
